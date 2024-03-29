a1f62c621f81c950622a1a50da78789b159e3dba
Vitor Savian <vitorandresaviandev@gmail.com>
2024-03-29 03:06:31 -0300

    C and Cpp initial commit

---
+++ data-structures/c/sort/insertion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int arr[] = {5, 2, 1, 11, 6, 9, 30};
+  int return = insertionSort(*arr);
+  printf("Hello world\n");
+  return EXIT_SUCCESS;
+}
+
+int insertionSort(int *arr) { return 0; }
