c69c902ac473f5f53ff5a4af17ab7fd4c7b3b014
Vitor Savian <vitorandresaviandev@gmail.com>
2024-07-09 19:05:46 -0300

    Add 1.1 and 1.2

---
+++ learncpp/1.2-Comments/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int int main (int argc, char *argv[]) {
+  std::cout << "Hello World!!" << std::endl; // Everything from here to the end of the line is ignored
+  return 0;
+}
