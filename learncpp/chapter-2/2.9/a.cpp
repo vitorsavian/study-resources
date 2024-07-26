a3089f5ca863bd8881e74ed37c36db39a7a5f37e
Vitor Savian <vitorandresaviandev@gmail.com>
2024-07-25 21:48:53 -0300

    Finished 2.9

---
+++ learncpp/chapter-2/2.9/a.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void myFcn(int x) { std::cout << x; }
---
+++ learncpp/chapter-2/2.9/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void myFcn(int x) { std::cout << 2 * x; }
+
+int main() { return 0; }
